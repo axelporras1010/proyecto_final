0fcfa8ea8949081c0098ed9188216e82e018a787
Mafer <cachoporojasmariafernanda@gmail.com>
2024-02-06 20:56:40 -0400

    mafer-pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,32 +9,28 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
         <!-- Styles -->
+        <link href="{{ asset('css/estilos.css') }}" rel="stylesheet" />
+
         <style>
-             /* ! tailwindcss v3.2.4 | MIT License | https://tailwindcss.com */*,::after,::before{font-family: "Montserrat", sans-serif;font-size:1em}
-        </style>
+            /* ! tailwindcss v3.2.4 | MIT License | https://tailwindcss.com */*,::after,::before{font-family: "Montserrat", sans-serif;} </style>
     </head>
-    <body class="antialiased">
+    <body class="body-personalizado">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
-                <div class="mr-500px p-10px ">
+                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                     @auth
                         <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
                     @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-                        <!-- boton de registro -->
-                        <!-- @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif -->
+                        <a href="{{ route('login') }}" class="links">Iniciar Sesion</a>
                     @endauth
                 </div>
             @endif
-
-             <div>
-                <p  class="bg-primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto qui dolore fugiat consectetur quisquam eaque quibusdam, reprehenderit sit a pariatur molestiae veritatis dicta fuga assumenda minus possimus voluptatum. Ratione, ea!</p>
-             </div>
+        </div>
+        <div class="center">
+            <img src="{{ asset('img/Logo VarHor Negativo.jpg') }}" alt="" class="LogoHome">
+        </div>
     </body>
 </html>
