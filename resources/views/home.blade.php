4861fccdd5e0e9ac7cbb14cc968a76afc56e1282
Mafer <cachoporojasmariafernanda@gmail.com>
2024-02-07 22:47:20 -0400

    mafer-ultimo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,10 +15,10 @@
                             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
-                                        <img class="d-block w-100" src="{{ asset('img/DSC01150.jpg') }}" alt="First slide" style="max-height: 400px;">
+                                        <img class="d-block w-100" src="{{ asset('img/DSC01150.jpg') }}" alt="First slide" style="max-height: 500px;">
                                     </div>
                                     <div class="carousel-item">
-                                        <img class="d-block w-100" src="{{ asset('img/DSC01164.jpg') }}" alt="Second slide" style="max-height: 400px;">
+                                        <img class="d-block w-100" src="{{ asset('img/DSC01164.jpg') }}" alt="Second slide" style="max-height: 500px;">
                                     </div>
                                 </div>
                                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -31,7 +31,7 @@
                                 </a>
                             </div>
                         </div>
-                        <p class="card-text">
+                        <p class="card-text mt-5">
                             En Yoka's Music Academy ofrecemos clases de canto de alta calidad impartidas por profesionales con amplia experiencia en la industria musical.
                         </p>
                         <p class="card-text">
