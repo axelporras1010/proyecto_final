1ceacda6eb37be6452aeb4116fdc36354f0d87f0
Axel Porras <axelporrasg@gmail.com>
2024-02-07 20:09:47 -0400

    logo-pestana

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Yeka Coach</title>
+    <!-- <link rel="icon" href="{{ asset('logo-sinfondo.png') }}" type="image/x-icon" /> -->
+
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -15,11 +17,6 @@
 
     <!-- Estilos personalizados -->
     <link href="{{ asset('css/estilos.css') }}" rel="stylesheet" />
-
-    <!-- Estilos adicionales -->
-    <style>
-        /* Aquí puedes agregar estilos personalizados si lo deseas */
-    </style>
 </head>
 <body class="body-personalizado">
     <div class="wrapper">
@@ -126,7 +123,7 @@
             <p>&copy; {{ date('Y') }} Yeka Coach Academy. Todos los derechos reservados.</p>
         </div>
     </footer>
-    <!-- Bootstrap JS (opcional, si lo necesitas) -->
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-YFzn/yN5BSOcmeFyplGdEOv9HTzD4CqRLNXnYGiJRt/HfL9RvV1+m5Lc2JPJ3qfW" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shCk+1Qr44pCW6vrbOTy4f2JfaWjyo7I5BdiD" crossorigin="anonymous"></script>
